6e118c37386b73073e554de685c9ed8357bc4988
peterpang <peterpang55@users.noreply.github.com>
2016-09-27 20:43:31 +0800

    Update config.php

--- pwechat/config/config.php
+++ pwechat/config/config.php
@@ -1,11 +1,12 @@
 <?php
 return new \Phalcon\Config(array(
+
     'database' => array(
         'adapter' => 'Mysql',
         'host' => '127.0.0.1',
         'username' => 'root',
-        'password' => 'yourpassword',
-        'dbname' => 'databasename',
+        'password' => 'password',
+        'dbname' => 'dbname',
         'charset' => 'utf8'
     ),
     'application' => array(
@@ -15,7 +16,22 @@ return new \Phalcon\Config(array(
         'viewsDir' => __DIR__ . '/../views/',
         'cacheDir' => APP_PATH . '/apps/cache/',
         'servicesDir' =>APP_PATH .'/apps/services',
-        'encryptKey'  =>'your own cryptKey',
+        'encryptKey'  =>'your encrykey',
         'baseUri' => '/pwechat/'
+    ),
+    'redis'  =>array(
+        'host'       =>'127.0.0.1',
+        'port'       =>'6379',
+        'auth'       =>'your auth',
+    ),
+    'weixin'  =>array(
+        'base_address'      =>'your appliaction url',
+        'appid'             =>'appid',
+        'appsecret'         =>'appsecret',
     )
+
+
 ));
+
+
+?>
