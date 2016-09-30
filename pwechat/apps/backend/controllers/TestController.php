1d43c194abe3ddfa8be37a9cab45bfd338cb67f6
pangxb <10846295@qq.com>
2016-09-30 10:23:11 +0800

    aaaa

---
+++ pwechat/apps/backend/controllers/TestController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pangxb
+ * Date: 16/9/29
+ * Time: 上午10:46
+ */
+
+namespace Wechat\Admin\Controllers;
+
+use Phalcon\Mvc\Controller;
+use Phalcon\Logger;
+use Phalcon\Logger\Adapter\File as FileAdapter;
+use Phalcon\Http\Request;
+
+class TestController extends ControllerBase
+{
+    protected $logger;
+    public function initialize(){
+        $this->logger = new FileAdapter(__DIR__ . '/../../logs/test.log');
+        $this->logger->setLogLevel(
+            Logger::CRITICAL
+        );
+    }
+   public function testAction(){
+       echo 'test Action';
+   }
+}
